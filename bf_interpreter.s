9f3d1695ac3fd9ec8b41bbadf6912e35eb75c38d
Serghei <serghei.mihailov@labs42.io>
2018-12-05 10:40:21 +0100

    Executing single instructions

--- bf_interpreter.s
+++ bf_interpreter.s
@@ -1,41 +1,125 @@
-.text
-prompt: .asciz "bf: "
-formatin: .asciz "%d"
+.data
+intro: .asciz "\n------------------------------\n\x1b[91m.-<[\x1b[95m brain-4k interpreter \x1b[91m]>+,    \x1b[0m \n------------------------------\n"
+prompt: .asciz "\x1b[96m> bf: \x1b[0m"
+format_command: .asciz " %lc"
+format_num_in: .asciz "%ld"
+format_line: .asciz " %s"
 formatout: .asciz "%d \n"
 
-plus:
+program: .zero 30000 # space for the program
+cells: .zero 30000 # cells for execution
 
-minus:
+word: .byte 1
 
-dot:
+# The eight language commands each consist of a single character:
+# Character 	Meaning
+# > 	increment the data pointer (to point to the next cell to the right).
+# < 	decrement the data pointer (to point to the next cell to the left).
+# + 	increment (increase by one) the byte at the data pointer.
+# - 	decrement (decrease by one) the byte at the data pointer.
+# . 	output the byte at the data pointer.
+# , 	accept one byte of input, storing its value in the byte at the data pointer.
+# [ 	if the byte at the data pointer is zero, then instead of moving the instruction pointer forward to the next command, jump it forward to the command after the matching ] command.
+# ] 	if the byte at the data pointer is nonzero, then instead of moving the instruction pointer forward to the next command, jump it back to the command after the matching [ command.
+# Source: Wikipedia
 
-comma:
+.text
 
-square_br_r:
+jumptable:
+  pushq %rbp
+  movq %rsp, %rbp
 
-square_br_l:
+  cmp $'+', %rdi
+  je plus
+  cmp $'-', %rdi
+  je minus
+  cmp $'.', %rdi
+  je dot
+  cmp $',', %rdi
+  je comma
+  cmp $']', %rdi
+  je angle_br_r
+  cmp $'[', %rdi
+  je angle_br_l
+  cmp $'>', %rdi
+  je angle_br_r
+  cmp $'<', %rdi
+  je angle_br_l
 
-angle_br_r:
+  jmp end
 
-angle_br_l:
+  plus: # +
+    incq (%rbp)
+    jmp jumptable_end
+
+  minus: # -
+    decq (%rbp)
+    jmp jumptable_end
+
+  dot: # .
+    movq (%rbp), %rsi
+    movq $formatout, %rdi
+    movq $0, %rax
+    call printf
+    jmp jumptable_end
+
+  comma: # ,
+    leaq (%rbp), %rsi
+    movq $format_num_in, %rdi
+    movq $0, %rax
+    call scanf
+    jmp jumptable_end
+
+  square_br_r: # ]
+    cmpq $0, (%rbp)
+    jmp jumptable_end
+  # jump back to the command after [ if non-zero
+
+  square_br_l: # [
+    cmpq $0, (%rbp)
+    jmp jumptable_end
+  # jump to the command after ] if zero
+
+  angle_br_r: # >
+    addq $word, %rbp
+    jmp jumptable_end
+
+  angle_br_l: # <
+    subq $word, %rbp
+    jmp jumptable_end
+
+jumptable_end:
+  movq %rbp, %rsp
+  popq %rbp
+  ret
 
 .global main
 
 main:
-  loop:
-    // initialize stack pointer
-    movq %rsp, %rbp
+  # initialize stack pointer
+  movq %rsp, %rbp
 
-    // prompt input from user
+  movq $intro, %rdi
+  movq $0, %rax
+  call printf
+
+  loop:
+    # prompt input from user
     movq $prompt, %rdi
     movq $0, %rax
     call printf
 
-    // allocate space for input and read input
+    # allocate space for input and read input
     subq $8, %rsp
     leaq (%rsp), %rsi
-    movq $formatin, %rdi
+    movq $format_command, %rdi
     movq $0, %rax
     call scanf
 
+    movq (%rsp), %rdi
+
+    call jumptable
+    call loop
+
+  end:
   call exit
